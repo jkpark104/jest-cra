8975999d74332d8c4dc449851feb12424089076b
jkpark104 <jkrang104@gmail.com>
2023-11-01 22:01:53 +0900

    feat: form, user-event

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,8 @@
 import "./App.scss";
-import logo from "./logo.svg";
+import SummaryForm from "./pages/summary/SummaryForm";
 
 function App() {
-  return (
-    <div data-testid="app" className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <SummaryForm></SummaryForm>;
 }
 
 export default App;
